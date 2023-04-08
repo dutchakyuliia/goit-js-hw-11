ddb52a1bf787e81425759282d74baa960340dcfd
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-04-08 15:12:15 +0200

    resovle task

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
 
-async function fetchImages(value, page) {
+async function fetchImage(value, page) {
     
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '35164644-c0ded33a4bfd222370a5029bc';
@@ -16,8 +16,8 @@ const findEL = `?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizont
 const response = await axios.get(`${BASE_URL}${findEL}`)
 return response;
     }
-    catch { (error) {
-    console.log(error)}
+    catch  (error) {
+    console.log(error)
     }
 };
 
@@ -32,3 +32,108 @@ let lightbox = new SimpleLightbox('.gallery a', {
 form.addEventListener('submit', handlerSubmitform);
 loadMoreBtnEl.addEventListener('click', handlerLoadMoreBtn);
 
+function createMarkup(data) {
+  let markupPost = data
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="photo-card" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b></br>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b></br>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b></br>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b></br>${downloads}
+    </p>
+  </div>
+</a>`;
+      }
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markupPost);
+}
+
+async function handlerSubmitform(e) {
+  e.preventDefault();
+  clearForm();
+  searchQuery = e.currentTarget.searchQuery.value.trim();
+  if (searchQuery === '') {
+    Notiflix.Notify.failure('Sorry, you have not entered anything.');
+    return;
+  } else {
+    try {
+      const response = await fetchImage(searchQuery, currentPage);
+      if (response.data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
+      );
+      createMarkup(response.data.hits);
+      lightbox.refresh();
+
+      if (response.data.hits.length === 40) {
+        loadMore();
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+async function handlerLoadMoreBtn(e) {
+  e.preventDefault();
+  try {
+    const response = await fetchImage(searchQuery, currentPage);
+    createMarkup(response.data.hits);
+    lightbox.refresh();
+    const { height: cardHeight } =
+      gallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    const totalPage = response.data.totalHits / 40;
+    if (totalPage <= currentPage) {
+      loadMoreBtnEl.classList.add('hidden');
+      Notiflix.Notify.success(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+
+    }
+    currentPage += 1;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function clearForm() {
+  gallery.innerHTML = '';
+  currentPage = 1;
+  searchQuery = null;
+  loadMoreBtnEl.classList.add('hidden');
+}
+function loadMore() {
+  currentPage += 1;
+  loadMoreBtnEl.classList.remove('hidden');
+}
